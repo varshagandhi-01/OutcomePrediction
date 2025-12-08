e6b5071cb38d7067b403d752dafa5d745f344654
varshagandhi-01 <varshagandhi@gmail.com>
2025-12-08 13:19:36 +0530

    test for mongodebconnection

--- demo.py
+++ demo.py
@@ -5,10 +5,23 @@ import os
 mongo_db_url = os.getenv('MONGODB_URL')
 print(mongo_db_url)
 '''
-
+'''
 # test data ingestion
 from outcome_prediction.pipeline.training_pipeline import TrainingPipeline
 
 obj = TrainingPipeline()
 obj.run_pipeline()
 
+'''
+
+from pymongo.mongo_client import MongoClient
+import os
+uri = os.getenv("MONGODB_URL")
+# Create a new client and connect to the server
+client = MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
